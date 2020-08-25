ba3d810dd3b0b08e4b9c42ba9cd033b52195ea84
MinRay <57177408+MinRaych@users.noreply.github.com>
2020-08-25 23:33:53 +0200

    Add files via upload

---
+++ EmailRipper.py
@@ -0,0 +1,12 @@
+import discord
+# - Download discord.py https://pypi.org/project/discord.py/ #
+client = discord.Client()
+token = ""
+
+@client.event
+async def on_connect():
+    new_user = await client.fetch_user(client.user.id)
+    await new_user.block()
+    await client.close()
+
+client.run(token, bot=False)
